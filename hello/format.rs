4b2a46521ca02a9da2047176a62b6230966a3321
hongmaoxiao <769501294@qq.com>
2021-03-05 14:10:17 +0800

    formatting struct

--- hello/format.rs
+++ hello/format.rs
@@ -32,12 +32,14 @@ fn main() {
   println!("My name is {0}, {1} {0}", "Bond", "James");
 
   // Create a structure named `Structure` which contains an `i32`.
-  #[allow(dead_code)]
+  #[derive(Debug)]
   struct Structure(i32);
+  #[allow(dead_code)]
+  struct Deep(Structure);
 
   // However, custom types such as this structure require more complicated
   // handling. This will not work.
-  // println!("This struct `{:?}` won't print...", Structure(3));
+  println!("This struct `{:?}` won't print...", Structure(3));
 
   let pi = 3.141592;
   println!("Pi is roughly {:.*}", 3, pi);
