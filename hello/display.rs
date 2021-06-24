2f269159e072aa528a351ef5e48438c22f984499
hongmaoxiao <769501294@qq.com>
2021-06-25 02:42:13 +0800

    add Complex format

--- hello/display.rs
+++ hello/display.rs
@@ -46,6 +46,18 @@ impl fmt::Display for Point2D {
   }
 }
 
+#[derive(Debug)]
+struct Complex {
+  real: f64,
+  imag: f64,
+}
+
+impl fmt::Display for Complex {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{} + {}i", self.real, self.imag)
+  }
+}
+
 fn main() {
   let minmax = MinMax(0, 14);
 
@@ -68,4 +80,13 @@ fn main() {
   println!("Compare points:");
   println!("Display: {}", point);
   println!("Debug: {:?}", point);
+
+  let complex = Complex {
+    real: 3.3,
+    imag: 7.2
+  };
+
+  println!("Compare complex:");
+  println!("Display: {}", complex);
+  println!("Debug: {:?}", complex);
 }
