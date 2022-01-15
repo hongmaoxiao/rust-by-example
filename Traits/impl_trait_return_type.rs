cfe6cb31a86a7bd4de966e21e40313da1c1432ec
hongmaoxiao <769501294@qq.com>
2022-01-15 20:56:57 +0800

    add Traits impl trait return type and argument type again

--- Traits/impl_trait_return_type.rs
+++ Traits/impl_trait_return_type.rs
@@ -19,6 +19,19 @@ fn combine_vecs(
   v.into_iter().chain(u.into_iter()).cycle()
 }
 
+// Returns a function that adds `y` to its input
+fn make_adder_function(y: i32) -> impl Fn(i32) -> i32 {
+  let closure = move |x: i32| { x + y };
+  closure
+}
+
+fn double_positives<'a>(numbers: &'a Vec<i32>) -> impl Iterator<Item = i32> + 'a {
+  numbers
+      .iter()
+      .filter(|x| x > &&0)
+      .map(|x| x * 2)
+}
+
 fn main() {
   let v1 = vec![1, 2, 3];
   let v2 = vec![4, 5];
@@ -30,4 +43,11 @@ fn main() {
   assert_eq!(Some(4), v3.next());
   assert_eq!(Some(5), v3.next());
   println!("all done");
+
+  let plus_one = make_adder_function(1);
+  assert_eq!(plus_one(2), 3);
+
+  let singles = vec![-3, -2, 2, 3];
+  let doubles = double_positives(&singles);
+  assert_eq!(doubles.collect::<Vec<i32>>(), vec![4, 6]);
 }
