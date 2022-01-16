2dbcb160ac177b123cfc80c349037d56d5a58099
hongmaoxiao <769501294@qq.com>
2022-01-17 02:46:01 +0800

    add Error Handling using result in main

---
+++ ErrorHandling/using_result_in_main.rs
@@ -0,0 +1,13 @@
+use std::num::ParseIntError;
+
+fn main() -> Result<(), ParseIntError> {
+  let number_str = "10";
+
+  let number = match number_str.parse::<i32>() {
+    Ok(number) => number,
+    Err(e) => return Err(e),
+  };
+
+  println!("{}", number);
+  Ok(())
+}
